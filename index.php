e46ac753aca747eec00b52dd6e462dd6fb90e92b
Tipa Koder <44733288+tipakoder@users.noreply.github.com>
2021-04-29 09:54:38 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+require_once "init.php";
+
+try{
+    $route = getRoute(load_config("apiRoutes"));
+
+    if( $route !== false ){
+        load_controller($route);
+    } else {
+        load_error(404, "Страница не найдена");
+    }
+}catch(Exception $e){
+    load_error(422, "Проблемы с загрузкой модулей");
+}
